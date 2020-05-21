25312d541bb746a3b0bc1660222514b5987a685c
coopervk <coopervk@gmail.com>
2020-05-21 02:22:44 -0500

    Add shutdown command to bot

--- cb.py
+++ cb.py
@@ -1,17 +1,27 @@
 import asyncio
 import os
+from telethon import TelegramClient, events
 import logging
-from telethon import TelegramClient
+#logging.basicConfig(level=logging.DEBUG)
 
-async def main():
-    me = await client.get_me()
+async def shutdown_switch(event):
+    print("Message received:", event.raw_text)
+    if 'sid' in event.raw_text:
+        print('Shutting down')
+        await event.client.disconnect()
 
-    # Send messages
-    await client.send_message(me, "test")
-
-if __name__ == "__main__":
-    # Init
+def main():
     ID = os.environ["TELEGRAM_API_ID"]
     hs = os.environ["TELEGRAM_API_HASH"]
     with TelegramClient('gdynamics', ID, hs) as client:
-        client.loop.run_until_complete(main())
+        print("Bot started")
+
+        # Register events
+        client.add_event_handler(shutdown_switch, events.NewMessage())
+        print("Events added")
+
+        # Run bot
+        client.run_until_disconnected()
+
+if __name__ == "__main__":
+    main()
