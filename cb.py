2be9a3d32a0f329f102c5c96503479748e87703a
coopervk <coopervk@gmail.com>
2020-05-21 15:55:35 -0500

    Move bot into object for state management

--- cb.py
+++ cb.py
@@ -2,43 +2,58 @@ import asyncio
 import os
 from telethon import TelegramClient, events
 import logging
-#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
-perms = {}
+class cb:
+    def __init__(self):
+        # Permissions
+        self.perms =    {
+                            "shutdown_switch":  {int(os.environ["TELEGRAM_OWNER_ID"])},
+                            "source_code":      {"ALL"}
+                        }
 
-def perm(wrapped_handler):
-    async def handler(event):
-        auth = perms[wrapped_handler.__name__]
-        if auth == {"ALL"} or event.message.from_id in auth:
-            await wrapped_handler(event)
-    return handler
-
-@perm
-async def shutdown_switch(event):
-    print('Shutting down')
-    await event.client.disconnect()
-
-@perm
-async def source_code(event):
-    await event.reply("https://github.com/coopervk/cb")
-
-def main():
-    # Permissions
-    perms["shutdown_switch"] = {os.environ["TELEGRAM_OWNER_ID"]}
-    perms["source_code"] = {"ALL"}
-
-    ID = os.environ["TELEGRAM_API_ID"]
-    hs = os.environ["TELEGRAM_API_HASH"]
-    with TelegramClient('gdynamics', ID, hs) as client:
+        ID = os.environ["TELEGRAM_API_ID"]
+        hs = os.environ["TELEGRAM_API_HASH"]
+        self.client = TelegramClient('gdynamics', ID, hs)
         print("Bot started")
 
-        # Register events
-        client.add_event_handler(shutdown_switch, events.NewMessage(pattern=';sid', chats=288400190))
-        client.add_event_handler(source_code, events.NewMessage(pattern=';source'))
-        print("Events added")
+    def perm(wrapped_handler):
+        print("permission check")
+        async def handler(self, event):
+            auth = self.perms[wrapped_handler.__name__]
+            print(auth, "vs", event.message.from_id)
+            if auth == {"ALL"} or event.message.from_id in auth:
+                print("permitted")
+                await wrapped_handler(self, event)
+            else:
+                print("denied")
+        return handler
 
-        # Run bot
-        client.run_until_disconnected()
+    @perm
+    async def shutdown_switch(self, event):
+        print('Shutting down')
+        await event.client.disconnect()
+
+    @perm
+    async def source_code(self, event):
+        print("source_code")
+        await event.reply("https://github.com/coopervk/cb")
+
+    async def literally_everything(self, event):
+        print("DEBUG:", event)
+
+    def run(self):
+        with self.client:
+            # Register events
+            print("Adding events")
+            self.client.add_event_handler(self.shutdown_switch, events.NewMessage(pattern=';sid', chats=288400190))
+            self.client.add_event_handler(self.source_code, events.NewMessage(pattern=';source'))
+            #self.client.add_event_handler(self.literally_everything)
+            print("Events added")
+
+            # Run bot
+            self.client.run_until_disconnected()
 
 if __name__ == "__main__":
-    main()
+    bot = cb()
+    bot.run()
