a6e54e147feb15e1fcade20e7b2815cec520029e
Cooper <coopervk@gmail.com>
2020-09-11 23:46:42 -0500

    Attempt to generalize and test against pictures with and without exif

--- exifdata.py
+++ exifdata.py
@@ -1,7 +1,19 @@
 import exif
 
-with open('samplegpsexif.jpg', 'rb') as image:
-    image = exif.Image(image)
-    print("dir:", dir(image))
-    print(f"gps lat, long: {image.gps_latitude}, {image.gps_longitude}")
+print("WITH EXIF")
 
+def get_exif_data(image_name):
+    with open(image_name, 'rb') as image:
+        image = exif.Image(image)
+        print("dir:", dir(image))
+        print(f"gps lat, long: {image.gps_latitude}, {image.gps_longitude}")
+
+def main():
+    print("---EXIF---")
+    get_exif_data("samplegpsexif.jpg")
+    print("---NO EXIF---")
+    get_exif_data("samplenoexif.jpg")
+
+
+if __name__ == "__main__":
+    main()
