04f254dcb8b18c83ade27ffbb56586b2151d282f
Cooper <coopervk@gmail.com>
2020-09-13 14:49:30 -0500

    Go into document

--- cb.py
+++ cb.py
@@ -365,7 +365,7 @@ class cb:
             return
         elif not isinstance(event.message.media, tl.types.MessageMediaDocument):
             await self.fmt_reply(event, "Did not send image as file!")
-        elif not event.message.media.mime_type != "image\jpeg":
+        elif not event.message.media.document.mime_type != "image\jpeg":
             await self.fmt_reply(event, "This bot only supports JPEG/.jpg")
 
         try:
