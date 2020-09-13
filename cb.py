3dc855fba168ce4ec5233bfb3aaef2e578bdfbc1
Cooper <coopervk@gmail.com>
2020-09-13 13:10:28 -0500

    == not = for conditionals

--- cb.py
+++ cb.py
@@ -359,9 +359,9 @@ class cb:
         if len(cmd) != 2:
             await self.fmt_reply(event, "Improper syntax for exif!")
 
-        if cmd[1] = "clean":
+        if cmd[1] == "clean":
             await self.fmt_reply(event, "*clean image and repost it back at you*")
-        elif cmd[1] = "details":
+        elif cmd[1] == "details":
             await self.fmt_reply(event, "*post all exif data on file*")
         else:
             await self.fmt_reply(event, "Improper syntax for exif!")
