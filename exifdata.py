64081ccd5dee36ab2378c39dd2e7e24e1f89e008
Cooper <coopervk@gmail.com>
2020-09-12 22:39:30 -0500

    Add detect for exif data presence

--- exifdata.py
+++ exifdata.py
@@ -1,17 +1,17 @@
 import exif
 
-print("WITH EXIF")
-
 def get_exif_data(image_name):
     with open(image_name, 'rb') as image:
         image = exif.Image(image)
         print("dir:", dir(image))
-        print(f"gps lat, long: {image.gps_latitude}, {image.gps_longitude}")
+        print("have exif?:", image.has_exif)
+        if image.has_exif:
+            print(f"gps lat, long: {image.gps_latitude}, {image.gps_longitude}")
 
 def main():
     print("---EXIF---")
     get_exif_data("samplegpsexif.jpg")
-    print("---NO EXIF---")
+    print("\n---NO EXIF---")
     get_exif_data("samplenoexif.jpg")
 
 
