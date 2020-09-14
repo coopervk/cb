cf2a52f14d950b7e75e1f8e51220eead95cb99be
Cooper <coopervk@gmail.com>
2020-09-13 22:32:42 -0500

    Add functionality for going through and getting all exif data

--- exifdata.py
+++ exifdata.py
@@ -1,12 +1,18 @@
 import exif
+import types
 
 def get_exif_data(image_name):
     with open(image_name, 'rb') as image:
         image = exif.Image(image)
-        print("dir:", dir(image))
-        print("have exif?:", image.has_exif)
         if image.has_exif:
-            print(f"gps lat, long: {image.gps_latitude}, {image.gps_longitude}")
+            for prop in dir(image):
+                try:
+                    val = getattr(image, prop)
+                    if prop[0:1] != "_":
+                        if not isinstance(val, types.MethodType):
+                            print(f"{prop}: {val}")
+                except:
+                    pass
 
 def clean_exif(image_name):
     with open(image_name, 'rb') as image:
@@ -23,10 +29,10 @@ def main():
     get_exif_data("samplegpsexif.jpg")
     print("\n---NO EXIF---")
     get_exif_data("samplenoexif.jpg")
-    print("\n---CLEAN EXIF---")
-    new_image = clean_exif("samplegpsexif.jpg")
-    if new_image:
-        get_exif_data(new_image)
+    #print("\n---CLEAN EXIF---")
+    #new_image = clean_exif("samplegpsexif.jpg")
+    #if new_image:
+    #    get_exif_data(new_image)
 
 if __name__ == "__main__":
     main()
