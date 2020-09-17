89ac3eeef0eafbc38f625053cae5d97438fa0342
Cooper <coopervk@gmail.com>
2020-09-16 22:22:27 -0500

    debug

--- cb.py
+++ cb.py
@@ -475,7 +475,7 @@ class CoopBoop:
         await self.fmt_reply(event, method.__doc__)
 
         for event in self.client.list_event_handlers():
-            print(event)
+            print(dir(event))
 
     async def literally_everything(self, event):
         """ Displays every single event the bot encounters for debugging or brainstorming
