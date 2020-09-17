e00893f5d38d2a0eb4f6519ed2dc7ad852efa0c0
Cooper <coopervk@gmail.com>
2020-09-16 23:07:03 -0500

    Debug

--- cb.py
+++ cb.py
@@ -163,6 +163,7 @@ class CoopBoop:
         for event_handler in self.client.list_event_handlers():
             mapping[event_handler[1].pattern] = event_handler[0]
             print(f"dir(pattern): {dir(event_handler[1].pattern)}")
+            print(f"type(pattern): {type(event_handler[1].pattern)}")
         return mapping
 
     @perm
